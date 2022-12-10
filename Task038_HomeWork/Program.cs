970a418ed042a23e5e5299761d785ca15ff27300
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-10 11:29:03 +0800

    add Task 038 HomeWork

---
+++ Task038_HomeWork/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным
+// и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+Console.WriteLine("Введите размер массива");
+ int size = int.Parse(Console.ReadLine());
+double[] arr = new double[size];
+double min = arr[0];
+double max = arr[0];
+
+
+ArrayRandomNumbers(arr);
+PrintArray(arr);
+
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    else if (arr[i] < min)
+    {
+        min = arr[i];
+    }
+}
+
+
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+double[] ArrayRandomNumbers(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }return array;
+}
+
+double[] PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }return array;
+    Console.WriteLine();
+}
